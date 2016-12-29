3a69c96cdff615544327b4160133df20d38d6ed5
Eric Miller <eric.miller@spirent.com>
2016-12-28 17:32:27 -0800

    Remove areopagus, b2evolution, and MyJournal

--- areopagus-3.0.7-hide/cache/data_cfg_template_QHST.php
+++
@@ -1,4 +0,0 @@
-<?php exit; ?>
-1313532821
-165
-a:5:{s:4:"name";s:4:"QHST";s:9:"copyright";s:23:"&copy; www.theology.edu";s:7:"version";s:5:"1.0.0";s:17:"template_bitfield";s:4:"lNg=";s:8:"filetime";i:1281996701;}
