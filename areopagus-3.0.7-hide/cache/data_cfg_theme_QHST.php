0240d8b62ea355f4df9abc6d7d3a3e7825d00d56
Eric Miller <eric.miller@spirent.com>
2016-12-28 17:07:10 -0800

    First attempt to align eric data with new website

--- areopagus-3.0.7-hide/cache/data_cfg_theme_QHST.php
+++
@@ -1,4 +0,0 @@
-<?php exit; ?>
-1313470806
-155
-a:5:{s:4:"name";s:4:"QHST";s:9:"copyright";s:23:"&copy; www.theology.edu";s:7:"version";s:5:"1.0.0";s:14:"parse_css_file";b:1;s:8:"filetime";i:1281934275;}
--- forum/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbms = 'mysql';
-$dbhost = 'localhost';
-$dbname = 'mysql96fe486663823ad3a7a3d7f6610';
-$dbuser = 'usrba81b9f006de';
-$dbpasswd = 'heretic';
-$table_prefix = 'phpbb_';
-define('PHPBB_INSTALLED', true);
-?>
